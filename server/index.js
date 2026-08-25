bbdf22332b7b7a7e367aa22aa4138a389d85d8eb
ResortRRG <resortreservationgroup@gmail.com>
2026-08-25 18:55:06 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import { registerPayslipRoutes } from "./payslipRoutes.js";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "25mb" }));
 app.use(cookieParser());
 
 registerAuthRoutes(app);
